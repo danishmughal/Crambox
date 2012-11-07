91d263f63dd488135ab8671b10097c68d057ce9e
Danish Mughal <mughal.danish@gmail.com>
2012-11-06 19:01:56 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampusLink::Application.config.secret_token = 'd7580934609ea293af4da012f51dac9a6b521f54e7ddb0418d61e75c8cb61d3e34e48f1dbea3c19247c1bebfb0bdbd7b4adf7dd97aec10a6a0cf47e8050e8d24'
